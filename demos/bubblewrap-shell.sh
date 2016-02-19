db1782164efb7e245c9dd22d94694b08c282c931
Alexander Larsson <alexl@redhat.com>
2016-02-19 12:11:43 +0100

    Refresh bubblewrap-shell demo
    This adds host (ro) resolv.conf, minimal passwd/group and sets
    XDG_RUNTIME_DIR.

--- demos/bubblewrap-shell.sh
+++ demos/bubblewrap-shell.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 # Use bubblewrap to run /bin/sh in the host's rootfs.
 set -euo pipefail
-exec bwrap --mount-ro-bind /usr /usr \
+PASSWD=`mktemp`
+getent passwd `id -u` 65534 > ${PASSWD}
+
+GROUP=`mktemp`
+getent group `id -g` 65534 > ${GROUP}
+
+(   # Remove temporary files before calling bwrap, they are open in the fds anyway
+    rm $GROUP
+    rm $PASSWD
+    bwrap --mount-ro-bind /usr /usr \
 	   --make-dir /tmp \
 	   --mount-proc /proc \
 	   --mount-dev /dev \
+	   --mount-ro-bind /etc/resolv.conf /etc/resolv.conf \
+	   --make-file 11 /etc/passwd \
+	   --make-file 12 /etc/group \
 	   --make-symlink usr/lib /lib \
 	   --make-symlink usr/lib64 /lib64 \
 	   --make-symlink usr/bin /bin \
 	   --make-symlink usr/sbin /sbin \
-	   --make-dir /run/user/$(id -u) \
 	   --chdir / \
 	   --unshare-pid \
-	   /bin/sh
+	   --make-dir /run/user/$(id -u) \
+	   --setenv XDG_RUNTIME_DIR "/run/user/`id -u`" \
+	   /bin/sh) 11< ${PASSWD} 12< ${GROUP}
