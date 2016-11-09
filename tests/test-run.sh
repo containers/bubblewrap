d43752a2ef8a5534471c7bf6815130c3a3b77572
Alexander Larsson <alexl@redhat.com>
2016-11-09 13:48:54 +0100

    Add test with basic running operations
    This runs a set of very basic operations that use the host root fs as
    the sandbox root. Ensuring that we're at least able to start a sandbox
    with various options, and that a setuid bwrap can't read files that
    should otherwise not be readable.
    
    Note, this SKIPs the test instead of FAILing if the most basic
    operation doesn't work, because our test suite doesn't support
    running the setuid test.
    
    Closes: #116
    Approved by: cgwalters

---
+++ tests/test-run.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+set -xeuo pipefail
+
+srcd=$(cd $(dirname $0) && pwd)
+bn=$(basename $0)
+tempdir=$(mktemp -d /var/tmp/tap-test.XXXXXX)
+touch ${tempdir}/.testtmp
+function cleanup () {
+    if test -n "${TEST_SKIP_CLEANUP:-}"; then
+        echo "Skipping cleanup of ${test_tmpdir}"
+    else if test -f ${tempdir}/.test; then
+        rm "${tempdir}" -rf
+    fi
+    fi
+}
+trap cleanup EXIT
+cd ${tempdir}
+
+skip () {
+    echo $@ 1>&2; exit 77
+}
+
+assert_not_reached () {
+    echo $@ 1>&2; exit 1
+}
+
+assert_file_has_content () {
+    if ! grep -q -e "$2" "$1"; then
+        echo 1>&2 "File '$1' doesn't match regexp '$2'"; exit 1
+    fi
+}
+
+FUSE_DIR=
+for mp in `cat /proc/self/mounts | grep " fuse[. ]" | grep user_id=1000 | awk '{print $2}'`; do
+    if test -d $mp; then
+        echo Using $mp as test fuse mount
+        FUSE_DIR=$mp
+        break
+    fi
+done
+
+# This is supposed to be an otherwise readable file in an unreadable (by the user) dir
+UNREADABLE=/root/.bashrc
+if test -x `dirname $UNREADABLE`; then
+    UNREADABLE=
+fi
+
+# Default arg, bind whole host fs to /, tmpfs on /tmp
+BWRAP="test-bwrap --bind / / --tmpfs /tmp"
+
+if ! $BWRAP true; then
+    skip Seems like bwrap is not working at all. Maybe setuid is not working
+fi
+
+#TODO: More ALTs when remount fixed:  "--unshare-pid" "--unshare-user --unshare-pid"
+for ALT in "" "--unshare-user" ; do
+    # Test fuse fs as bind source
+    if [ x$FUSE_DIR != x ]; then
+        $BWRAP $ALT  --proc /proc --dev /dev --bind $FUSE_DIR /tmp/foo true
+    fi
+    # no --dev => no devpts => no map_root workaround
+    $BWRAP $ALT --proc /proc true
+    # No network
+    $BWRAP $ALT --unshare-net --proc /proc --dev /dev true
+    # Unreadable file
+    echo -n "expect EPERM: "
+    if $BWRAP $ALT --unshare-net --proc /proc --bind /etc/shadow  /tmp/foo cat /etc/shadow; then
+        assert_not_reached Could read /etc/shadow
+    fi
+    # Unreadable dir
+    if [ x$UNREADABLE != x ]; then
+        echo -n "expect EPERM: "
+        if $BWRAP $ALT --unshare-net --proc /proc --dev /dev --bind $UNREADABLE  /tmp/foo cat /tmp/foo ; then
+            assert_not_reached Could read $UNREADABLE
+        fi
+    fi
+done
+echo OK
