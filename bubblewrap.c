f9f6127474591c8a4731d8b755a4670adcb83b9e
Alexander Larsson <alexl@redhat.com>
2019-11-21 18:25:35 +0100

    setuid mode: Properly drop privs in monitor and pid1
    It turns out we have this check in drop_privs():
    
     if (getuid () == 0 && setuid (opt_sandbox_uid) < 0)
    
    Which is supposed to drop back to the regular uid in the case
    we're in setuid mode and we're in the monitor_child() or do_init()
    processes.
    
    Unfortunately we're setuid, not plain root, so uid is not 0, but euid is zero.
    
    This caused the monitoring processes to be running partially as root
    which shows up weird in /proc.
    
    Fix this by checking euid for 0 instead.

--- bubblewrap.c
+++ bubblewrap.c
@@ -822,7 +822,7 @@ drop_privs (bool keep_requested_caps)
 {
   assert (!keep_requested_caps || !is_privileged);
   /* Drop root uid */
-  if (getuid () == 0 && setuid (opt_sandbox_uid) < 0)
+  if (geteuid () == 0 && setuid (opt_sandbox_uid) < 0)
     die_with_error ("unable to drop root uid");
 
   drop_all_caps (keep_requested_caps);
