a6c90c516bf28152bf08ee91b8bf4f240aee236d
Simon McVittie <smcv@collabora.com>
2021-08-31 11:54:10 +0100

    Add SPDX-License-Identifier for files that already specify license
    This is a step towards REUSE compliance. Third-party files that we do
    not otherwise edit (git.mk, m4/attributes.m4) are excluded here.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- bind-mount.h
+++ bind-mount.h
@@ -1,5 +1,6 @@
 /* bubblewrap
  * Copyright (C) 2016 Alexander Larsson
+ * SPDX-License-Identifier: LGPL-2.0-or-later
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
