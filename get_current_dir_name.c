47cf000f1a9d66eb9e6f001b8da5e4d323297c50
Dallas Strouse <93224879+orowith2os@users.noreply.github.com>
2023-05-04 12:24:41 -0500

    Forgot to comment the license

--- get_current_dir_name.c
+++ get_current_dir_name.c
@@ -1,4 +1,4 @@
-Copyright © 2005-2020 Rich Felker, et al., 2023 Dallas Strouse
+/* Copyright © 2005-2020 Rich Felker, et al., 2023 Dallas Strouse
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation files (the
@@ -18,7 +18,7 @@ IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
 CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
+*/
 
 #include <stdlib.h>
 #include <string.h>
