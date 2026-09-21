containers/bubblewrap

commit fd7399760ac36695dc438095f0ac80cbac6c11f1

Add a mechanism to enable debug() at compile-time
Now that bubblewrap doesn't support being setuid, we can safely look
at environment variables to decide whether to emit noisy debug messages.
Instead of inventing our own environment variable, let's use
`DEBUG_INVOCATION` as popularized by systemd.

Enable debug messages in one CI pass, leaving them disabled in the
other.

Signed-off-by: Simon McVittie <smcv@collabora.com>

utils.c
@@ -39,6 +39,10 @@
 #define security_check_context(x) security_check_context ((security_context_t) x)
 #endif
 
+#ifdef BWRAP_DEBUG
+bool bwrap_is_debugging = false;
+#endif
+
 bool bwrap_level_prefix = false;
 int proc_fd = -1;
 
@@ -48,6 +52,9 @@ bwrap_logv (int severity,
             va_list args,
             const char *detail)
 {
+  if (severity == LOG_DEBUG && !bwrap_is_debugging)
+    return;
+
   if (bwrap_level_prefix)
     fprintf (stderr, "<%d>", severity);
 

.github/workflows/check.yml
@@ -26,7 +26,7 @@ jobs:
       run: mkdir test-logs
     - name: setup
       run: |
-        meson setup _build -Db_sanitize=address,undefined -Doptimization=2
+        meson setup _build -Db_sanitize=address,undefined -Ddebug_logging=true -Doptimization=2
       env:
         CFLAGS: >-
           -Wp,-D_FORTIFY_SOURCE=2
@@ -38,11 +38,13 @@ jobs:
         ./_build/bwrap --bind / / --tmpfs /tmp true
       env:
         ASAN_OPTIONS: detect_leaks=0
+        DEBUG_INVOCATION: '1'
     - name: test
       run: |
         BWRAP_MUST_WORK=1 meson test -C _build
       env:
         ASAN_OPTIONS: detect_leaks=0
+        DEBUG_INVOCATION: '1'
     - name: Collect overall test logs on failure
       if: failure()
       run: mv _build/meson-logs/testlog.txt test-logs/ || true

bubblewrap.c
@@ -2920,6 +2920,11 @@ main (int    argc,
   /* Get the (optional) privileges we need */
   acquire_privs ();
 
+#ifdef BWRAP_DEBUG
+  if (getenv ("DEBUG_INVOCATION") != NULL)
+    bwrap_is_debugging = true;
+#endif
+
   /* Never gain any more privs during exec */
   if (prctl (PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0) < 0)
     die_with_error ("prctl(PR_SET_NO_NEW_PRIVS) failed");

meson.build
@@ -87,6 +87,10 @@ if selinux_dep.found()
   endif
 endif
 
+if get_option('debug')
+  cdata.set('BWRAP_DEBUG', 1)
+endif
+
 assume_kernel = get_option('assume_kernel')
 if assume_kernel != ''
   kernel_parts = assume_kernel.split('.')

utils.h
@@ -33,7 +33,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-#if 0
+#ifdef BWRAP_DEBUG
+extern bool bwrap_is_debugging;
 #define debug(...) bwrap_log (LOG_DEBUG, __VA_ARGS__)
 #else
 #define debug(...)

commit bd5329c8dcb1a272dbee541efd8094911a7e727e

utils: Fix the build with debugging disabled
When debug-level log messages weren't enabled at compile-time, we
can't look at `bwrap_is_debugging`, because it doesn't exist.

Fixes: fd73997 "Add a mechanism to enable debug() at compile-time"
Signed-off-by: Simon McVittie <smcv@collabora.com>

utils.c
@@ -52,8 +52,10 @@ bwrap_logv (int severity,
             va_list args,
             const char *detail)
 {
+#ifdef BWRAP_DEBUG
   if (severity == LOG_DEBUG && !bwrap_is_debugging)
     return;
+#endif
 
   if (bwrap_level_prefix)
     fprintf (stderr, "<%d>", severity);